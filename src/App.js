1afcfa70895a9b7ff630266b99c382731a91ed40
Stoodkev <quentin.correa@gmail.com>
2018-10-16 16:40:39 +0800

    Construction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Site under construction.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://steemit.com/@steem-plus"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            SteemPlus Blog
           </a>
         </header>
       </div>
