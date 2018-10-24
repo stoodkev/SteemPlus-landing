1ece4988f46fc92fa76645536bbcde7bb67a7931
Cedric Guillas <cedric.guillas78@gmail.com>
2018-10-24 15:04:30 +0800

    Updated size footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+const style = {
+  marginTop: "3rem",
+  marginBottom: "3rem"
+};
+
 const Footer = () => (
-  <div className="footer">© 2018 Cedric Guillas and Quentin Correa</div>
+  <div className="footer" style={style}>
+    © 2018 Cedric Guillas and Quentin Correa
+  </div>
 );
 
 export default Footer;
